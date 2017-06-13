c72236d26c8e47403fd4a36d93027e75d2222ea9
suffiasameera <suffiasameera12@gmail.com>
2017-06-13 13:22:47 +0530

    Create IncidentServices.go

--- itops/vendor/github.com/ibm/itops/services/IncidentServices.go
+++ itops/vendor/github.com/ibm/itops/services/IncidentServices.go
@@ -13,7 +13,7 @@ import (
 	"github.com/ibm/itops/data"
 )
 
-var mapIncident map[string]data.IncidentDo
+var mapIncident map[string]data.IncidentD
 
 /*
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
